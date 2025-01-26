39002ae4d798bd028cafef15add43d7566fcf80e
Rajath Shettigar <111166897+rajathshttgr@users.noreply.github.com>
2025-01-26 10:13:30 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:7001/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://rbac-backend-ten.vercel.app/api" || "http://localhost:7001/api";
 
 export default BASE_URL;
